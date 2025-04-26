c03ec67d1faf21842f2363e0519f9f2f7ff044f9
Dalsaetra <mattis.dalsaetra@gmail.com>
2025-04-26 16:44:02 -0400

    Start of input integration

---
+++ python/input_integration.py
@@ -0,0 +1,21 @@
+import numpy as np
+from synapse_dynamics import SynapseDynamics
+
+class InputIntegration:
+    def __init__(self, synapse_dynamics: SynapseDynamics):
+        """
+        InputIntegration class to represent the input integration of a neuron population.
+        """
+        self.synapse_dynamics = synapse_dynamics
+
+    def __call__(self, neurons_V, I_ext=None):
+        """
+        neurons_V: n_neurons x 1
+        Returns: n_neurons x 1
+        """
+        # Get the synaptic input from the synapse dynamics
+        I_syn = self.synapse_dynamics(neurons_V)
+        # Add the external input if provided
+        if I_ext is not None:
+            I_syn += I_ext
+        return I_syn
