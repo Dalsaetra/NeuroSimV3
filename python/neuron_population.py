71f3dc54c917a5e44cc6479c528dc153e553d60f
Dalsaetra <mattis.dalsaetra@gmail.com>
2025-04-27 19:57:28 -0400

    Microcolumn analysis

--- python/neuron_population.py
+++ python/neuron_population.py
@@ -2,18 +2,18 @@ import numpy as np
 from neuron_templates import neuron_type_IZ
 
 class NeuronPopulation:
-    def __init__(self, layer_distribution, neuron_distribution, layer_distances, neuron_types, 
+    def __init__(self, neurons_per_layer, neuron_distribution, layer_distances, neuron_types, 
                  inhibitory, threshold_decay, delta_V=2.5, bias=0.0, threshold_mult=1.05, n_params=13):
         """
-        layer_distribution: list of integers, number of neurons in each layer
-        neuron_distribution: list with length layer_distribution of np.arrays of length neuron_types, probabilities of each neuron type in each layer
+        neurons_per_layer: list of integers, number of neurons in each layer
+        neuron_distribution: list with length neurons_per_layer of np.arrays of length neuron_types, probabilities of each neuron type in each layer
         layer_distances: matrix of distances between layers, shape (n_layers, n_layers)
-        neuron_types: list of neuron types (string), keys for types un neuron_templates.py
+        neuron_types: list of neuron types (string), keys for types in neuron_templates.py
         inhibitory: list of booleans, True if the neuron is inhibitory, False if it is excitatory, shape as neuron_types
         n_params: number of parameters for each neuron, default is 13 (k, a, b, d, C, Vr, Vt, Vpeak, c, delta_V, bias, threshold_mult, threshold_decay)
         """
 
-        self.layer_distribution = layer_distribution
+        self.neurons_per_layer = neurons_per_layer
         self.neuron_distribution = neuron_distribution
         self.layer_distances = layer_distances
         self.neuron_types = neuron_types
@@ -22,8 +22,8 @@ class NeuronPopulation:
         self.n_neuron_types = len(neuron_types)
 
         # Initialize the neuron population
-        self.n_layers = len(layer_distribution)
-        self.n_neurons = sum(layer_distribution)
+        self.n_layers = len(neurons_per_layer)
+        self.n_neurons = sum(neurons_per_layer)
 
         self.neuron_population = np.zeros((self.n_neurons, self.n_params))
         self.inhibitory_mask = np.zeros((self.n_neurons), dtype=bool)
@@ -42,8 +42,8 @@ class NeuronPopulation:
         for i in range(self.n_layers):
             layer_indices_layer = []
             self.dist_norm = self.neuron_distribution[i] / np.sum(self.neuron_distribution[i])
-            for j in range(self.layer_distribution[i]):
-                index = sum(self.layer_distribution[:i]) + j
+            for j in range(self.neurons_per_layer[i]):
+                index = sum(self.neurons_per_layer[:i]) + j
                 layer_indices_layer.append(index)
                 # Get the neuron type for this neuron
                 # Normalize distribution to sum to 1
